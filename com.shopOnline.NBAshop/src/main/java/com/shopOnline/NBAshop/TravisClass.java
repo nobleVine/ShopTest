7fecf0bc5e1b7a7bf27c52eb647c4579658870dc
Marco Vignini <marco.vignini@stud.unifi.it>
2017-07-07 13:51:10 +0200

    Commit di setup: travis, configurazione ecc..

---
+++ com.shopOnline.NBAshop/src/main/java/com/shopOnline/NBAshop/TravisClass.java
@@ -0,0 +1,7 @@
+package com.shopOnline.NBAshop;
+
+public class TravisClass {
+	public String getStringTravis(){
+		return "Travis";
+	}
+}
---
+++ com.shopOnline.NBAshop/src/test/java/com/shopOnline/NBAshop/TravisClassTest.java
@@ -0,0 +1,15 @@
+package com.shopOnline.NBAshop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TravisClassTest {
+
+	@Test
+	public void test() {
+		TravisClass t = new TravisClass();
+		assertEquals("Travis", t.getStringTravis());
+	}
+
+}
